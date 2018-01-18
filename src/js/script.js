32a76e9af923e5397d13b618392231a7a4ba6af3
入江信之介 <(kiyoshidainagon@gmail.com)>
2018-01-18 09:17:59 +0900

    webGLのko-dowoisyoku

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,65 @@
 import Sample from './lib/Sample';
 import $ from 'jquery';
 
-const sample = new Sample({
-    name: 'world'
-});
+const ratio = window.devicePixelRatio || 1
 
-$('.wrapper').on('click', () => {
-    console.log(`hello, ${sample.name}.`);
-});
+// 1 rendererを作る
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+})
+renderer.setPixelRatio(ratio)
+
+const content = document.querySelector('.content')
+const canvas = renderer.domElement
+wrapper.appendChild(canvas)
+
+// 2 sceneを作る
+const scene = new THREE.Scene()
+
+// 3 cameraを作る
+const camera = new THREE.PerspectiveCamera(
+    50,
+    1,
+    1,
+    10000
+)
+camera.position.set(0, 100, 400)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+scene.add(camera)
+
+// 4 lightを作る
+const directionalLight = new THREE.DirectionalLight(0xffffff)
+directionalLight.position.set(0, 200, 800)
+scene.add(directionalLight)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, .4)
+scene.add(ambientLight)
+
+// 5 meshを作る
+const boxGeometry = new THREE.BoxGeometry(80, 80, 80)
+const boxMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff
+}) 
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
+boxMesh.position.set(0, 0, 0)
+boxMesh.rotation.y = -20 * Math.PI / 180
+scene.add(boxMesh)
+
+onWindowResize()
+window.addEventListener('resize', onWindowResize)
+requestAnimationFrame(tick)
+
+function onWindowResize() {
+    const width = wrapper.offsetWidth
+    const height = wrapper.offsetHeight
+    
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    
+    renderer.setSize(width, height)
+}
+
+function tick() {
+    renderer.render(scene, camera)
+    requestAnimationFrame(tick)
+}
